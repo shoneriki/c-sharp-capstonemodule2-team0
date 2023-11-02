e29dc1447c4452f753413816512b6955559de79f
Preston Johnson <preston1of2@gmail.com>
2023-11-02 16:10:33 +0000

    Preston Johnson

---
+++ TenmoServer/Controllers/TransferController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TenmoServer.Controllers
+{
+    [Authorize]
+    [Route("transfer")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        //private ITransferDao transferDao;
+    }
+}
