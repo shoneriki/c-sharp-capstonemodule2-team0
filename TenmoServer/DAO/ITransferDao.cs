074c95804c77e91d512e2a6af48511a27d091b57
Sho Neriki <ikirenohs@gmail.com>
2023-11-02 14:44:29 -0400

    C:/Program Files/git/users/{user_id}/accounts endpoint working on postman

--- TenmoServer/DAO/ITransferDao.cs
+++ TenmoServer/DAO/ITransferDao.cs
@@ -11,17 +11,5 @@ namespace TenmoServer.DAO
         Transfer CreateTransfer(Transfer transfer);
         Transfer GetTransferById(int transferId);
         List<Transfer> GetTransfersOfUser(int userId);
-
-        // TODO: TransferMoney (give money aka lose money and balance decreases)
-        //public decimal TransferMoneyToUser(decimal money);
-        //bool TransferMoney(int accountId, decimal amount);
-<<<<<<< HEAD
-        //// TODO: ReceiveMoney (get money aka receive money and balance increases)
-        ////public decimal GettingMoneyFromUser(decimal money);
-=======
-        // TODO: ReceiveMoney (get money aka receive money and balance increases)
-        //public decimal GettingMoneyFromUser(decimal money);
->>>>>>> 7ba8ad4165d838718fc1191bf3cd2c2c1750a8c1
-        //bool ReceiveMoney(int accountId, decimal amount);
     }
 }
