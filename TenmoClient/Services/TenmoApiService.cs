84fee87616a0b884404260bff9144bd534a49f05
Sho Neriki <ikirenohs@gmail.com>
2023-11-03 12:05:24 -0400

    changed some of TenmoApiService and updated TransferSqlDao further again

--- TenmoClient/Services/TenmoApiService.cs
+++ TenmoClient/Services/TenmoApiService.cs
@@ -43,7 +43,7 @@ namespace TenmoClient.Services
             return account.Balance;
         }
 
-        public List<ApiUser> GetUser()
+        public List<ApiUser> GetUsers()
         {
             RestRequest request = new RestRequest("users");
             IRestResponse<List<ApiUser>> response = client.Get<List<ApiUser>>(request);
@@ -67,7 +67,116 @@ namespace TenmoClient.Services
             return response.Data;
         }
 
-        
+        /* 
+
+*/
+
+        public Transfer CreateTransfer(Transfer transfer)
+        {
+            RestRequest request = new RestRequest("transfer/create");
+            request.AddJsonBody(transfer);
+            IRestResponse<Transfer> response = client.Post<Transfer>(request);
+
+            CheckForError(response);
+
+            //if (response.IsSuccessful)
+            //{
+            //    decimal updatedBalance = GetBalance();
+            //}
+            return response.Data;
+        }
+
+        public Transfer UpdateTransfer(Transfer transfer)
+        {
+            RestRequest request = new RestRequest($"transfer/update");
+            request.AddJsonBody(transfer);
+            IRestResponse<Transfer> response = client.Put<Transfer>(request);
+
+            CheckForError(response);
+            return response.Data;
+        }
+
+        public List<Transfer> GetTransferListOfUser(int user_id)
+        {
+            RestRequest request = new RestRequest($"transfer/users/{user_id}");
+            IRestResponse<List<Transfer>> response = client.Get<List<Transfer>>(request);
+
+            CheckForError(response);
+            return response.Data;
+        }
+
+        public Transfer GetTransferDetails(int transferId)
+        {
+            RestRequest request = new RestRequest($"transfer/{transferId}");
+            IRestResponse<Transfer> response = client.Get<Transfer>(request);
+
+            CheckForError(response);
+            return response.Data;
+        }
+
+        //public List<Transfer> GetPendingTransfersForUser(int userId)
+        //{
+        //    RestRequest request = new RestRequest($"transfer/users/{userId}/pending");
+        //    IRestResponse<List<Transfer>> response = client.Get<List<Transfer>>(request);
+
+        //    CheckForError(response);
+        //    return response.Data;
+        //}
+
+
+        //public TransferType SendMoney(TransferType transferType)
+        //{
+        //    RestRequest request = new RestRequest($"transfer/{transferType.TypeDesc}");
+        //    IRestResponse<TransferType> response = client.Put<TransferType>(request);
+        //    CheckForError(response);
+        //    return response.Data;
+
+        //}
+
+        //public TransferType RequestMoney(TransferType transferType)
+        //{
+        //    RestRequest request = new RestRequest($"transfer/{transferType.TypeDesc}");
+        //    IRestResponse<TransferType> response = client.Put<TransferType>(request);
+        //    CheckForError(response);
+        //    return response.Data;
+
+        //}
+
+        //public List<Transfer> GetPendingTransfers(int transfer_status_id)
+        //{
+        //    List<Transfer> transfers = new List<Transfer>();
+        //    string sql = "SELECT * FROM transfer " +
+        //        "WHERE transfer_status_id = @transfer_status_id";
+
+        //    try
+        //    {
+        //        using (SqlConnection conn = new SqlConnection(connectionString))
+        //        {
+        //            conn.Open();
+        //            SqlCommand cmd = new SqlCommand(sql, conn);
+        //            cmd.Parameters.AddWithValue("@transfer_status_id", transfer_status_id);
+        //            SqlDataReader reader = cmd.ExecuteReader();
+
+        //            while (reader.Read())
+        //            {
+        //                Transfer transfer = MapRowToTransfer(reader);
+        //                transfers.Add(transfer);
+        //            }
+        //        }
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        throw new DaoException("SQL exception on GetPendingTransfers", ex); ;
+        //    }
+
+        //    return transfers;
+        //}
+
+
+        /* 
+
+         */
+
 
     }
 }
